a9c3f8f402527dbf7c38fbb3f613e5e3e4d3d0b4
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2023-11-19 09:56:51 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurações personalizadas de CORS
+  app.enableCors({
+    origin: ['*'], // ou '*' para permitir de qualquer origem
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   await app.listen(3000);
-  app.enableCors();
 }
+
 bootstrap();
